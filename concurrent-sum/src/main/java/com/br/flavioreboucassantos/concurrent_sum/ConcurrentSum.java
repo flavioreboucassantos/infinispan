3f2fcba3246728ed8fbf4e08d1090198078f446e
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2026-03-14 22:49:18 -0300

    Creates and Finalize First Project concurrent-sum

---
+++ concurrent-sum/src/main/java/com/br/flavioreboucassantos/concurrent_sum/ConcurrentSum.java
@@ -0,0 +1,103 @@
+package com.br.flavioreboucassantos.concurrent_sum;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.configuration.TransactionMode;
+import org.infinispan.client.hotrod.transaction.lookup.RemoteTransactionManagerLookup;
+
+public class ConcurrentSum {
+
+	/*
+	 * Change Here to Select Between runThreadSafeSumTask or runNonThreadSafeSumTask
+	 */
+	static public final boolean isThreadSafe = false;
+	
+	/*
+	 * Configurations of Concurrent Sum
+	 */
+	static private final String cacheName = "concurrent-sum";
+	static private final String keyName = "valor";
+
+	static private final int numberOfAdders = 5;
+	static private final int numberOfSums = 500;
+	static private final long nsTimeBetweenSums = 1000000 * 1; // 1000000 is 1 ms & * 1000 is 1 sec
+	static AtomicInteger countAddersConcluded = new AtomicInteger(0);
+
+	static private final RemoteCacheManager remoteCacheManager;
+	static private final RemoteCache<String, Integer> cache;
+
+	static {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+
+		cb.addServer()
+				.host("127.0.0.1")
+				.port(11222)
+				.security().authentication()
+				.username("admin")
+				.password("admin");
+
+		cb.remoteCache(cacheName)
+				.transactionManagerLookup(RemoteTransactionManagerLookup.getInstance())
+				.transactionMode(TransactionMode.NON_DURABLE_XA);
+
+		cb.transactionTimeout(60, TimeUnit.SECONDS);
+
+		remoteCacheManager = new RemoteCacheManager(cb.build());
+
+		cache = remoteCacheManager.getCache(cacheName);
+	}
+
+	static private void print(Object p) {
+		System.out.println(p);
+	}
+
+	static private boolean allAddersConcluded() {
+		return (countAddersConcluded.get() < numberOfAdders) ? false : true;
+	}
+
+	static public void main(String[] args) throws InterruptedException {
+//		Cache capabilities: Transactional
+//		Transaction mode: NON_DURABLE_XA - Infinispan disables transaction recovery and enlists the cache with the Java transaction manager through the XAResource API. Disabling recovery means that you cannot recover failed transactions but do not have the performance overhead of handling in-doubt transactions.
+//		Locking mode: Optimistic - Infinispan locks keys when it invokes the commit() method. Keys are locked for shorter periods of time which reduces overall latency but makes transaction recovery less efficient.
+
+// 		Storage Type: OFF_HEAP - Off-heap storage uses less memory per entry compared with JVM heap storage and can improve performance by avoiding avoids garbage collection (GC) runs.
+//		Concurrency level: 32 - Configures the number of locks to create in the shared pool for lock striping.
+//		Lock timeout: 10 ms - Sets how many milliseconds to wait for lock acquisition.
+//		Lock striping: true - Uses a shared pool of locks for all entries in the cache. Striping lowers the memory footprint for locks but can reduce concurrency. If you disable striping, a lock is created for each entry in the cache.
+
+//		Read isolation levels guarantee whether or not data in the cache has changed during a transaction.
+// 		Read isolation level: REPEATABLE_READ - Read operations return the same value that Infinispan initially retrieves for an entry during a transaction. This is the default read isolation level because it guarantees consistency.
+
+//		Stop timeout: 30000ms
+//		Complete timeout: 60000ms
+//		Reaper interval: 30000ms
+
+//		Cache configuration: concurrent-sum.json
+
+		cache.put("valor", 0);
+
+		for (int i = 0; i < numberOfAdders; i++) {
+			Thread thread = new Thread(new RunnableConcurrentSum(cache, keyName, numberOfSums, nsTimeBetweenSums));
+			thread.start();
+		}
+
+		while (!allAddersConcluded())
+			Thread.yield();
+
+		final int valorEsperado = numberOfAdders * numberOfSums;
+		final int valorEncontrado = cache.get(keyName);
+
+		print("Valor esperado: " + valorEsperado);
+		print("Valor encontrado: " + valorEncontrado);
+
+		remoteCacheManager.close();
+	}
+
+	static public void adderConcluded() {
+		countAddersConcluded.incrementAndGet();
+	}
+}
---
+++ concurrent-sum/src/main/java/com/br/flavioreboucassantos/concurrent_sum/RunnableConcurrentSum.java
@@ -0,0 +1,99 @@
+package com.br.flavioreboucassantos.concurrent_sum;
+
+import org.infinispan.client.hotrod.RemoteCache;
+
+import jakarta.transaction.SystemException;
+import jakarta.transaction.TransactionManager;
+
+public class RunnableConcurrentSum implements Runnable {
+
+	private final int numberOfSums;
+	private final long nsTimeBetweenSums;
+
+	final RemoteCache<String, Integer> cache;
+	private final String keyName;
+
+	/*
+	 * Non Thread Safe Sum Task
+	 */
+	private void nonThreadSafeSumTask() {
+		Integer integer = cache.get(keyName);
+		integer++;
+		cache.put("valor", integer);
+	}
+
+	private void runNonThreadSafeSumTask() {
+		// sum every nsTimeBetweenSums
+		int numberOfSum = 0;
+		while (numberOfSum++ < numberOfSums) {
+			nonThreadSafeSumTask();
+			final long timeNextRun = System.nanoTime() + nsTimeBetweenSums;
+//			System.out.println(numberOfSum + " >>> " + timeNextRun);
+			while (System.nanoTime() < timeNextRun)
+				Thread.yield();
+		}
+		ConcurrentSum.adderConcluded();
+	}
+
+	/*
+	 * Thread Safe Sum Task
+	 */
+	private boolean threadSafeSumTask() {
+		final TransactionManager tm = cache.getTransactionManager();
+
+		try {
+			tm.begin(); // (!) NÃO TRAVA
+
+			Integer integer = cache.get(keyName);
+			integer++;
+			cache.put("valor", integer);
+
+			tm.commit(); // O Infinispan verifica versões aqui
+
+			return true;
+		} catch (Exception e) {
+			try {
+				tm.rollback();
+			} catch (IllegalStateException | SecurityException | SystemException e1) {
+//				e1.printStackTrace();
+			}
+//			System.err.println("Conflito de concorrência: " + e.getMessage());
+//			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private void runThreadSafeSumTask() {
+		// sum every nsTimeBetweenSums
+		int numberOfSum = 0;
+		while (numberOfSum++ < numberOfSums) {
+			/*
+			 * Locking mode: Optimistic && Read isolation level: REPEATABLE_READ Optimistic trava a key quando UMA THREAD consegue commit, mas as outras THREADS já
+			 * receberam o valor DIRTY por não haver travado antes de READ. O DIRTY força o ROLLBACK e por isso é preciso retentar threadSafeSumTask()
+			 */
+			if (!threadSafeSumTask())
+				numberOfSum--;
+			final long timeNextRun = System.nanoTime() + nsTimeBetweenSums;
+//			System.out.println(numberOfSum + " >>> " + timeNextRun);
+			while (System.nanoTime() < timeNextRun)
+				Thread.yield();
+		}
+		ConcurrentSum.adderConcluded();
+	}
+
+	public RunnableConcurrentSum(RemoteCache<String, Integer> cache, final String keyName, final int numberOfSums, final long nsTimeBetweenSums) {
+		this.numberOfSums = numberOfSums;
+		this.nsTimeBetweenSums = nsTimeBetweenSums;
+
+		this.cache = cache;
+		this.keyName = keyName;
+	}
+
+	@Override
+	public void run() {
+		if (ConcurrentSum.isThreadSafe)
+			runThreadSafeSumTask();
+		else
+			runNonThreadSafeSumTask();
+	}
+}
